b0de3771f717291f7e7870c52ece037c4f9e45e2
dyalksmiya-source <dyalksmiya@gmail.com>
2026-08-22 01:31:57 +0200

    Update server.py

--- server.py
+++ server.py
@@ -101,5 +101,5 @@ def convert():
 
 
 if __name__ == "__main__":
-port = int(os.environ.get("PORT" , 5001))
-app.run(host="0.0.0.0" , port=port, debug=False, use_reloader=False)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
